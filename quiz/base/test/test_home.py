d5fe78d00c3b7e31c00b47c2686b35086c4dc12b
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-25 02:23:44 -0300

    Redenrizando a pagina incial.
    close #18

--- quiz/base/test/test_home.py
+++
@@ -1,8 +0,0 @@
-from http import HTTPStatus
-
-from django.test import Client
-
-
-def test_home_stattus_ok(client: Client):
-    response = client.get('/')
-    assert response.status_code == HTTPStatus.OK  # 200

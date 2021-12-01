0d24ddcb27bd822209d051029b298065a184ec3e
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-01 02:19:27 -0300

    Formulário para usuario já existente
    close #38

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render, redirect, reverse
 
 from quiz.base.forms import AlunoForm
-from quiz.base.models import Pergunta
+from quiz.base.models import Pergunta, Aluno
 
 
 def home(request):
+
     if request.method == 'POST':
-        form = AlunoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('base:pergunta',  kwargs={'slug': 1}))
+
+        # Usuario já existe
+        email = request.POST['email']
+        try:
+            aluno = Aluno.objects.get(email=email)
+        except Aluno.DoesNotExist:
+
+            # Usuário não existe
+            form = AlunoForm(request.POST)
+            if form.is_valid():
+                aluno = form.save()
+                request.session['aluno_id'] = aluno.id
+                return redirect(reverse('base:pergunta',  kwargs={'slug': 1}))
+            else:
+                contexto = {'formulario': form}
+                return render(request, 'base/home.html', contexto)
+
         else:
-            contexto = {'formulario': form}
-            return render(request, 'base/home.html', contexto)
+            request.session['aluno_id'] = aluno.id
+            return redirect(reverse('base:pergunta',  kwargs={'slug': 1}))
 
     return render(request, 'base/home.html')
 

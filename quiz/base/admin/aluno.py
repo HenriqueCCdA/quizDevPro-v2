af759efb49cbbe38a34304cc5314d29104a7879f
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-03 02:08:01 -0300

    Criando o modelo para as repostas.
    close #48

--- quiz/base/admin/aluno.py
+++ quiz/base/admin/aluno.py
@@ -5,4 +5,4 @@ from quiz.base.models import Aluno
 
 @admin.register(Aluno)
 class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'email')
+    list_display = ('id', 'nome', 'email', 'criado_em')
---
+++ quiz/base/models/pergunta_e_resposta.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from quiz.base.models import Aluno
+
+
+class Pergunta(models.Model):
+    enunciado = models.TextField()
+    disponivel = models.BooleanField(default=False)
+    alternativas = models.JSONField()
+    alternativas_correta = models.IntegerField(choices=[
+        (0, 'A'),
+        (1, 'B'),
+        (2, 'C'),
+        (3, 'D'),
+    ])
+
+    def __str__(self):
+        return self.enunciado
+
+
+class Resposta(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    pontos = models.IntegerField()
+    respondida_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['aluno', 'pergunta'], name='resposta_unica')
+        ]
+
+    def __str__(self):
+        return f'{self.aluno.email} {self.pergunta.enunciado}'

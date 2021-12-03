a4142e7b118f60ada1d021babd949c858175002d
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-03 01:11:20 -0300

    Redirecionamento para a pagina de classificação quando as perguntas acabam.
    close #46

--- quiz/base/tests/test_questions.py
+++ quiz/base/tests/test_questions.py
@@ -14,6 +14,11 @@ SLUG = 1
 
 @pytest.fixture
 def um_pergunta_db(db):
+    '''
+    Cria uma pergunta ativa
+    :param db: fixture padrão
+    :return: retorna uma pergunta ativa
+    '''
     alternatives = ['def', 'func', '@', 'if']
     return baker.make(Pergunta, disponivel=True, alternativas={'array': alternatives})
 
@@ -24,13 +29,40 @@ def response_sem_usuario_logado(client: Client, um_pergunta_db):
 
 
 @pytest.fixture
-def reponse_com_usuario_logado(client: Client, um_pergunta_db):
+def client_login_simpliciado(client: Client, db):
+    '''
+    Modifica a sessão do cliente para simular o login de usuario.
+    :param Client: fixture padrão
+    :param db: fixture padrão
+    :return: uma istancia de client com a session modificada
+    '''
     session = client.session
     session['aluno_id'] = 1
     session.save()
     session.save()
+    return client
 
-    return client.get(reverse('base:pergunta', kwargs={'slug': SLUG}))
+
+@pytest.fixture
+def reponse_com_usuario_logado(client_login_simpliciado: Client, um_pergunta_db):
+    '''
+    Cliente com um usuario logado com apenas um pergunta ativa. Este cliente acesse uma pergunta existente
+    :param client_login_simpliciado:
+    :param um_pergunta_db:
+    :return: response
+    '''
+    return client_login_simpliciado.get(reverse('base:pergunta', kwargs={'slug': SLUG}))
+
+
+@pytest.fixture
+def reponse_com_usuario_logado_alem_da_ultima_pergunta(client_login_simpliciado: Client, um_pergunta_db):
+    '''
+    Cliente com um usuario logado com apenas um pergunta ativa. Este cliente acesse uma pergunta não existente
+    :param client_login_simpliciado:
+    :param um_pergunta_db:
+    :return: reponse
+    '''
+    return client_login_simpliciado.get(reverse('base:pergunta', kwargs={'slug': SLUG+1}))
 
 
 @pytest.mark.parametrize(('slug', ), [
@@ -72,3 +104,12 @@ def testa_o_conteudo_pagina_da_pergunta_com_o_usuario_logado(reponse_com_usuario
     # Alternatives
     for alt in um_pergunta_db.alternativas['array']:
         assert_contains(reponse_com_usuario_logado, alt)
+
+
+def testa_o_redireionamento_para_a_pagina_de_classificao(reponse_com_usuario_logado_alem_da_ultima_pergunta):
+    '''
+    Testa o redirecionamento para a pagina de classifica quando tem se acessar uma pergunta fora do limite
+    '''
+
+    assert reponse_com_usuario_logado_alem_da_ultima_pergunta.status_code == HTTPStatus.FOUND  # 302
+    assert reponse_com_usuario_logado_alem_da_ultima_pergunta.headers['Location'] == reverse('base:classificacao')

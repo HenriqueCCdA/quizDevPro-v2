cfd3bb3b21f41938eea9e4e94c395cfd15c28b05
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-09 01:31:58 -0300

    Trocar o slug para inteiro na rota.
    close #44

--- quiz/base/tests/test_questions.py
+++ quiz/base/tests/test_questions.py
@@ -7,12 +7,12 @@ from django.urls import reverse
 from quiz.base.django_assertions import assert_contains
 
 
-SLUG = 1
+INDICE = 1
 
 
 @pytest.fixture
 def response_sem_usuario_logado(client: Client, uma_pergunta_db):
-    return client.get(reverse('base:pergunta', kwargs={'slug': SLUG}))
+    return client.get(reverse('base:pergunta', kwargs={'indice': INDICE}))
 
 
 @pytest.fixture
@@ -23,7 +23,7 @@ def response_com_usuario_logado(client_login_simpliciado: Client, uma_pergunta_d
     :param um_pergunta_db:
     :return: response
     '''
-    return client_login_simpliciado.get(reverse('base:pergunta', kwargs={'slug': SLUG}))
+    return client_login_simpliciado.get(reverse('base:pergunta', kwargs={'indice': INDICE}))
 
 
 @pytest.fixture
@@ -34,19 +34,19 @@ def response_com_usuario_logado_alem_da_ultima_pergunta(client_login_simpliciado
     :param um_pergunta_db:
     :return: reponse
     '''
-    return client_login_simpliciado.get(reverse('base:pergunta', kwargs={'slug': SLUG + 1}))
+    return client_login_simpliciado.get(reverse('base:pergunta', kwargs={'indice': INDICE + 1}))
 
 
-@pytest.mark.parametrize(('slug', ), [
+@pytest.mark.parametrize(('indice', ), [
     (1, ),
     (2, ),
 ])
-def testa_reverse_para_a_pagina_de_pergunta(slug):
+def testa_reverse_para_a_pagina_de_pergunta(indice):
     '''
     Testa a funcao reverse para pergunta
     :param slug:
     '''
-    assert f'/pergunta/{slug}' == reverse('base:pergunta', kwargs={'slug': slug})
+    assert f'/pergunta/{indice}' == reverse('base:pergunta', kwargs={'indice': indice})
 
 
 def testa_status_code_pagina_da_pergunta_sem_o_usuario_logado(response_sem_usuario_logado, db):
@@ -69,9 +69,9 @@ def testa_o_conteudo_pagina_da_pergunta_com_o_usuario_logado(response_com_usuari
     '''
     Testa o contuedo da paragina da pergunta com o usuario o usuario logado.
     '''
-    slug = SLUG
+    indice = INDICE
 
-    assert_contains(response_com_usuario_logado, f'<h2> Questão {slug} </h2>')
+    assert_contains(response_com_usuario_logado, f'<h2> Questão {indice} </h2>')
     assert_contains(response_com_usuario_logado, f'<h3>{uma_pergunta_db.enunciado}</h3>')
     # Alternatives
     for alt in uma_pergunta_db.alternativas['array']:
--- quiz/base/urls.py
+++ quiz/base/urls.py
@@ -5,6 +5,6 @@ from quiz.base import views
 app_name = 'base'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('pergunta/<slug:slug>', views.pergunta, name='pergunta'),
+    path('pergunta/<int:indice>', views.pergunta, name='pergunta'),
     path('classificacao/', views.classificacao, name='classificacao'),
 ]
